e1203e0926db7d12f5bab673117ea4d8933eeacc
Duy <trinhchanduybojack@gmail.com>
2025-01-29 10:31:57 +0700

    leetcode plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -70,6 +70,20 @@ return {
 
     { "MunifTanjim/nui.nvim", lazy = true },
     { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
+    -- leetcode plugin
+    {
+        "kawre/leetcode.nvim",
+        build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            -- "ibhagwan/fzf-lua",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {
+            -- configuration goes here
+        },
+    },
     -- Outmeal
     {
         "dustinblackman/oatmeal.nvim",
